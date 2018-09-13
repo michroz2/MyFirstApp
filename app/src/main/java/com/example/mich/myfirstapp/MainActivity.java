a46453f1bd5b702d0b9854f1c89cf9d285ede3b0
michroz2 <michroz@gmail.com>
2018-09-13 13:15:10 +0300

    added methods to record/stop/play

--- app/src/main/java/com/example/mich/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/mich/myfirstapp/MainActivity.java
@@ -6,8 +6,15 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.media.MediaRecorder;
+import android.media.MediaPlayer;
+import java.io.IOException;
 
 public class MainActivity extends Activity implements OnClickListener {
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+    private static String mFileName = null;
 
     private static final String TAG = "MainActivity";
 
@@ -15,11 +22,52 @@ public class MainActivity extends Activity implements OnClickListener {
     Button btnPlay;
     Button btnRec;
 
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "on create");
         setContentView(R.layout.activity_main);
+
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
 
         btnStop = findViewById(R.id.buttonStop);
         btnStop.setOnClickListener(this);
@@ -38,12 +86,14 @@ public class MainActivity extends Activity implements OnClickListener {
             case R.id.buttonPlay:
                 Log.d(TAG, "play");
                 // TODO: начать воспроизведение последней записи.
+                startPlaying();
                 // TODO: если записи нету, кнопка должна быть неактивна
                 // todo: import android.media.MediaPlayer в помощь
                 break;
             case R.id.buttonStop:
                 Log.d(TAG, "stop");
                 // todo: остановить запись
+                stopRecording();
                 // todo: если запись не воспроизводится и не записывается, то кнопка неактивна
 
                 break;
@@ -51,6 +101,7 @@ public class MainActivity extends Activity implements OnClickListener {
                 Log.d(TAG, "record");
                 // todo: сделать кнопку play неактивной
                 // todo: начать воспроизведение
+                startRecording();
                 // todo: android.media.MediaRecorder в помощь
                 break;
         }
