0a30c3206ad1457b5f8dc259cd7c67e91df2fa81
Andrei <andrei.prokofjev@gmail.com>
2018-09-17 17:26:53 +0300

    update state

--- app/src/main/java/com/example/mich/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/mich/myfirstapp/MainActivity.java
@@ -2,16 +2,16 @@ package com.example.mich.myfirstapp;
 
 import android.Manifest;
 import android.app.Activity;
-import android.os.Bundle;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.media.MediaRecorder;
-import android.media.MediaPlayer;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -23,10 +23,10 @@ public class MainActivity extends Activity implements OnClickListener {
     private MediaPlayer mPlayer = null;
     private static String mFileName = null;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-/*
-    private static boolean isRecording = false;
-    private static boolean isPlaying = false;
-*/
+    /*
+        private static boolean isRecording = false;
+        private static boolean isPlaying = false;
+    */
     private static final String TAG = "MainActivity";
 
     private String strMessage = getString(R.string.state_initial); //"START RECORDING";
@@ -41,17 +41,17 @@ public class MainActivity extends Activity implements OnClickListener {
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
-    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
+        switch (requestCode) {
             case REQUEST_RECORD_AUDIO_PERMISSION:
-                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                 break;
         }
-        if (!permissionToRecordAccepted ) finish();
+        if (!permissionToRecordAccepted) finish();
 
     }
 
@@ -70,44 +70,45 @@ public class MainActivity extends Activity implements OnClickListener {
         }
 
         mRecorder.start();
-        mActivityState = ActivityState.RECORDING;
+        updateState(ActivityState.RECORDING);
     }
 
     private void stopRecording() {
         mRecorder.stop();
         mRecorder.release();
         mRecorder = null;
-        mActivityState = ActivityState.STOPPED;
-
+        updateState(ActivityState.STOPPED);
     }
+
     private void startPlaying(String mFileName) {
         mPlayer = new MediaPlayer();
         try {
             mPlayer.setDataSource(mFileName);
             mPlayer.prepare();
             mPlayer.start();
-            mActivityState = ActivityState.PLAYING;
+            updateState(ActivityState.PLAYING);
             mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
                     stopPlaying();
-                    mActivityState = ActivityState.STOPPED;
-//TODO check if we need this here:
-                    updateState(mActivityState);
+                    //TODO check if we need this here:
+                    updateState(ActivityState.STOPPED);
                 }
             });
 
         } catch (IOException e) {
             Log.e(TAG, "prepare() failed");
+            updateState(ActivityState.INITIAL);
         }
     }
 
     private void stopPlaying() {
         mPlayer.release();
         mPlayer = null;
-
+        updateState(ActivityState.STOPPED);
     }
-//Тестовая процедура для вывода данных максимальной громкости медиарекордера (по UserInteraction, например) во время рекординга
+
+    //Тестовая процедура для вывода данных максимальной громкости медиарекордера (по UserInteraction, например) во время рекординга
     private void showRecAmplitude(MediaRecorder mRecorder) {
         if (mActivityState == ActivityState.RECORDING) {
             Integer intRecAmplitude = mRecorder.getMaxAmplitude();
@@ -121,6 +122,8 @@ public class MainActivity extends Activity implements OnClickListener {
     }
 
     private void updateState(ActivityState state) {
+
+        mActivityState = state;
 
         txtViewMessage.setText(getString(state.strId));
 
@@ -142,34 +145,33 @@ public class MainActivity extends Activity implements OnClickListener {
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
-        if (mActivityState == ActivityState.INITIAL) {
-            // Record to the external cache directory for visibility
-            mFileName = getExternalCacheDir().getAbsolutePath();
-            mFileName += "/audiorecordtest.3gp";
+
+        // Record to the external cache directory for visibility
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        mFileName += "/audiorecordtest.3gp";
 
 
-            btnStop = findViewById(R.id.buttonStop);
-            btnStop.setOnClickListener(this);
+        btnStop = findViewById(R.id.buttonStop);
+        btnStop.setOnClickListener(this);
 
-            btnPlay = findViewById(R.id.buttonPlay);
-            btnPlay.setOnClickListener(this);
+        btnPlay = findViewById(R.id.buttonPlay);
+        btnPlay.setOnClickListener(this);
 
-            btnRec = findViewById(R.id.buttonRec);
-            btnRec.setOnClickListener(this);
+        btnRec = findViewById(R.id.buttonRec);
+        btnRec.setOnClickListener(this);
 
 
-            ibtnStop = findViewById(R.id.imageButtonStop);
-            ibtnStop.setOnClickListener(this);
+        ibtnStop = findViewById(R.id.imageButtonStop);
+        ibtnStop.setOnClickListener(this);
 
-            ibtnPlay = findViewById(R.id.imageButtonPlay);
-            ibtnPlay.setOnClickListener(this);
+        ibtnPlay = findViewById(R.id.imageButtonPlay);
+        ibtnPlay.setOnClickListener(this);
 
-            ibtnRec = findViewById(R.id.imageButtonRec);
-            ibtnRec.setOnClickListener(this);
+        ibtnRec = findViewById(R.id.imageButtonRec);
+        ibtnRec.setOnClickListener(this);
 
-         }
 
-         updateState(mActivityState); //наверное это можно было и в скобках сверху сделать. но, возможно, не помешает лишний раз установить текущий стэйт
+        updateState(ActivityState.INITIAL); //наверное это можно было и в скобках сверху сделать. но, возможно, не помешает лишний раз установить текущий стэйт
 
     }
 
@@ -199,8 +201,6 @@ public class MainActivity extends Activity implements OnClickListener {
                 Log.d(TAG, "play");
                 // начать воспроизведение последней записи.
                 startPlaying(mFileName);
-                // установить кнопки:
-                mActivityState = ActivityState.PLAYING;
                 break;
 
             case R.id.buttonStop:
@@ -216,22 +216,16 @@ public class MainActivity extends Activity implements OnClickListener {
                         stopPlaying();
                         break;
                 }
-
-                mActivityState = ActivityState.STOPPED;
                 break;
 
             case R.id.buttonRec:
             case R.id.imageButtonRec:
                 Log.d(TAG, "record");
                 startRecording(mFileName);
-                mActivityState = ActivityState.RECORDING;
                 //TODO - запустить таймер и начать анализировать громкость
                 break;
 
         }
-
-        updateState(mActivityState);
-
     }
 
     @Override
@@ -239,35 +233,41 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onBackPressed();
         Log.d(TAG, "on BackPressed");
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "on Destroy");
     }
+
     @Override
     public void onContentChanged() {
         super.onContentChanged();
         Log.d(TAG, "on ContentChanged");
     }
+
     @Override
     public void onPause() {
         super.onPause();
         Log.d(TAG, "on Pause");
     }
+
     @Override
     public void onRestart() {
         super.onRestart();
         Log.d(TAG, "on Restart");
     }
+
     @Override
     public void onResume() {
         super.onResume();
         Log.d(TAG, "on Resume");
     }
+
     @Override
     public void onUserInteraction() {
         super.onUserInteraction();
- //       Log.d(TAG, "on UserInteraction");
+        //       Log.d(TAG, "on UserInteraction");
         showRecAmplitude(mRecorder);
     }
 
