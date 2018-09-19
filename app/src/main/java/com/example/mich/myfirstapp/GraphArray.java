9ed63f243421a4a4078b1a5dfdcdf608b9ab9d03
Andrei <andrei.prokofjev@gmail.com>
2018-09-19 10:06:39 +0300

    some comments added

--- app/src/main/java/com/example/mich/myfirstapp/GraphArray.java
+++ app/src/main/java/com/example/mich/myfirstapp/GraphArray.java
@@ -2,7 +2,11 @@ package com.example.mich.myfirstapp;
 
 public class GraphArray {
 
-    public int[] Data = new int[100];
+    // todo: переменная должна быть спрятана от других классов
+    public int[] Data = new int[100]; // TODO: поля в java должны начинаться с маленькой буквы. П
+
+    // TODO: 19-Sep-18 инициализацию data перенести в конструктор класса
+    // TODO: 19-Sep-18 массив заполнить случайными значениями от -100 до 100
 
     public void initArray() {
         for (int i = 0; i < Data.length; i++) {
@@ -10,7 +14,7 @@ public class GraphArray {
         }
     }
 
-    public Integer getData(int i) {
-        return Data[(i % Data.length)];
+    public Integer getData(int i) {       // TODO: у тебя массив int, значит и getData должна возвращать int, a  не Integer
+        return Data[(i % Data.length)];   // TODO: зачем брать остаток от деления на длину массива? достаточно return data[i];
     }
 }
